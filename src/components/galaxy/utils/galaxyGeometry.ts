4657adb6a30c7bef9333abf67a7f74adb189c63d
Suraj Kumar Singh <42529024+s0oraj@users.noreply.github.com>
2025-01-04 08:54:04 +0530

    Update galaxyGeometry.ts

--- src/components/galaxy/utils/galaxyGeometry.ts
+++ src/components/galaxy/utils/galaxyGeometry.ts
@@ -6,7 +6,7 @@ const DEFAULT_CONFIG: GalaxyConfig = {
   radius: 30,
   branches: 3, // More branches for realism
   spin: 1.0, // Reduced for more natural spiral
-  randomnessPower: 2.2,
+  randomnessPower: 3.5,
   bulgeSize: 0.25, // New: controls central bulge size
   armWidth: 0.5, // New: controls spiral arm width
   dustLanes: true, // New: enable dust lanes
