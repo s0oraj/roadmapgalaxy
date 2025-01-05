f775bcf33c472dbb1648b505c88092055824a197
suraj <ridesuraj@gmail.com>
2025-01-05 20:08:16 +0530

    modified commit

--- src/data/index.ts
+++ src/data/index.ts
@@ -5,10 +5,22 @@ import * as level3 from './level3';
 import * as level4 from './level4';
 
 export const levelData = {
-  1: level1,
-  2: level2,
-  3: level3,
-  4: level4,
+  1: {
+    nodes: level1.initialNodes,
+    edges: level1.initialEdges
+  },
+  2: {
+    nodes: level2.initialNodes,
+    edges: level2.initialEdges
+  },
+  3: {
+    nodes: level3.initialNodes,
+    edges: level3.initialEdges
+  },
+  4: {
+    nodes: level4.initialNodes,
+    edges: level4.initialEdges
+  }
 };
 
 export type LevelNumber = 1 | 2 | 3 | 4;
